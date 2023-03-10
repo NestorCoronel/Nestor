685c6d34364e9b050787754936b9dbcd66d2ea0f
Nestor <adidas156554@gmail.com>
2023-03-09 21:41:21 -0300

    Nuevo Archivo

---
+++ variables/src/main/java/com/mycompany/variables/Variables.java
@@ -0,0 +1,22 @@
+
+
+package com.mycompany.variables;
+
+
+public class Variables {
+
+    public static void main(String[] args) {
+        int num = 5 ;
+        int num2 = 10 ;
+        var nombre = "Nestor";
+        System. out.println (" Hola " + nombre) ;
+        int suma = num + num2;
+          System . out.println ("");
+        System . out.println ("La suma es " + suma );
+        int numero = 48;
+         double decimales = 3.55;
+       boolean bandera = false;
+       int vector [] ;
+     System . out.println ( "numero = " + numero) ;
+    }
+}
